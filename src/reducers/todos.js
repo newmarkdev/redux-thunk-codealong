1f4bd59fd74be1035720d0103122768582b45a58
newmarkdev <newmarkdev@gmail.com>
2021-04-27 15:32:11 +0200

    added ui and todos thunk

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit'
+import { ui } from './ui'
 
 export const todos = createSlice({
   name: 'todos',
@@ -11,3 +12,16 @@ export const todos = createSlice({
     }
   }
 })
+
+export const fetchTodos = () => {
+  return (dispatch) => {
+    dispatch(ui.actions.setLoading(true))
+    fetch('https://jsonplaceholder.typicode.com/users/1/todos')
+      .then((res) => res.json())
+      .then((json) => {
+        dispatch(todos.actions.setTodos(json))
+        dispatch(ui.actions.setLoading(false))
+      })
+  }
+}
+
